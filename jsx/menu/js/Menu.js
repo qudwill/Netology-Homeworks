8beebfd0fbe2a4398196f7f4bb5830f54dbe2a78
Vladimir Isaev <info@isaev.in>
2017-05-29 23:36:56 +0300

    Finished 2nd task - Menu

---
+++ jsx/menu/js/Menu.js
@@ -0,0 +1,24 @@
+const Menu = function(props) {
+  const { items, opened } = props;
+
+  if (opened) {
+    return (
+      <div className='menu menu-open'>
+        <div className='menu-toggle'>
+          <span></span>
+        </div>
+        <nav>
+          <ul>
+            {items.map((item) => <li><a href={item.href}>{item.title}</a></li>) }
+          </ul>
+        </nav>
+      </div>
+    );
+  } else {
+    return (
+      <div className='menu'>
+        <div className='menu-toggle'></div>
+      </div>
+    );
+  }
+}
