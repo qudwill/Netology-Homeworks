8a246e85daa98c5eacc499860d343183ab226a99
Vladimir Isaev <info@isaev.in>
2017-06-06 11:57:56 +0300

    fixed shop task

--- jsx/store/js/ShopItem.js
+++ jsx/store/js/ShopItem.js
@@ -1,36 +1,27 @@
 const ShopItem = function(props) {
   const { item } = props;
 
+  /**
+    * Деструктуризация
+    */ 
+  const { brand, title, description, descriptionFull, price, currency } = item;
+
   return (
     <div className='main-content'>
-      <h2>{ item.brand }</h2>
-      <h1>{ item.title }</h1>
-      <h3>{ item.description }</h3>
+      <h2>{ brand }</h2>
+      <h1>{ title }</h1>
+      <h3>{ description }</h3>
       <div className='description'>
-        { item.descriptionFull }
+        { descriptionFull }
       </div>
       <div className='highlight-window mobile'>
         <div className='highlight-overlay'></div>
       </div>
       <div className='divider'></div>
       <div className='purchase-info'>
-        <div className='price'>{ item.currency }{ item.price.toFixed(2) }</div>
+        <div className='price'>{ (currency === '₽') ? price.toFixed(2) + currency : currency + price.toFixed(2) }</div>
         <button>Добавить в корзину</button>
       </div>
     </div>
   );
 }
-
-const item = {
-  brand: 'Tiger of Sweden',
-  title: 'Leonard coat',
-  description: 'Minimalistic coat in cotton-blend',
-  descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
-  price: 399,
-  currency: '£'
-}
-
-ReactDOM.render(
-  <ShopItem item={item} />,
-  document.getElementById('root')
-);
